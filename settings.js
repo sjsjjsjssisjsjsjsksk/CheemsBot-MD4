0f1d8049c7faecde66f34614d904dd85866b4135
sjsjjsjssisjsjsjsksk <118295187+sjsjjsjssisjsjsjsksk@users.noreply.github.com>
2022-11-16 03:30:09 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -134,10 +134,10 @@ global.mess = {
    potionawal: 1
 }
 global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
-global.flaming = 'XeonMedia/theme/Cheems-bot.mp4")ht=500&fontsize=100&text='
-global.fluming = 'XeonMedia/theme/Cheems-bot.mp4")ht=500&fontsize=100&text='
-global.flarun = 'XeonMedia/theme/Cheems-bot.mp4")ht=500&fontsize=100&text='
-global.flasmurf = 'XeonMedia/theme/Cheems-bot.mp4")&text='
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
